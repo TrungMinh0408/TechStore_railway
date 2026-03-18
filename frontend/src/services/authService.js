ec73d53ab29f28f13594adfbbefd622303afbac7
TrungMinh <trungm0306@gmail.com>
2026-03-18 18:19:05 +0700

    Login Auth

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,13 +1,14 @@
 import api from "../utils/axios";
+const API = import.meta.env.VITE_API_URL;
 
 export const loginApi = (data) => {
-  return api.post("/users/login", data);
+  return api.post(`${API}/users/login`, data);
 };
 
 export const registerApi = (data) => {
-  return api.post("/users/register", data);
+  return api.post(`${API}/users/register`, data);
 };
 
 export const getMeApi = () => {
-  return api.get("/users/profile"); 
+  return api.get(`${API}/users/profile`);
 };
