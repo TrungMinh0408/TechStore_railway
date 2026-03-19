73aa2812232b76964fd931b1b19a6a0aea27b3dc
TrungMinh <trungm0306@gmail.com>
2026-03-19 13:04:22 +0700

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     proxy: {
 
       '/api': {
-        target: 'https://techstorerailway-copy-production.up.railway.app',
+        target: 'https://techstorerailway-production.up.railway.app',
         changeOrigin: true,
         secure: false,
       }
