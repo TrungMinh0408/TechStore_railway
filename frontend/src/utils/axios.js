bb5d8d7675a8985bbd9b4cea9dc414cb6f915cbc
TrungMinh <trungm0306@gmail.com>
2026-03-20 12:56:10 +0700

    axios

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://techstorerailway-production.up.railway.app",
+    "https://techstorerailway-production.up.railway.app/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
