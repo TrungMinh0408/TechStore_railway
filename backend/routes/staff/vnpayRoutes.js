99ac03d2e404ada422203b4e98f3a5c62a65af93
TrungMinh <trungm0306@gmail.com>
2026-03-21 22:23:05 +0700

    auth

--- backend/routes/staff/vnpayRoutes.js
+++ backend/routes/staff/vnpayRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { createPayment, vnpayIPN } from "../../controllers/staff/vnpayController.js";
 import authMiddleware from "../../middlewares/authMiddleware.js";
 
-// app.use(authMiddleware);
+app.use(authMiddleware);
 const router = express.Router();
 router.post("/create", createPayment);
 router.get("/ipn", vnpayIPN);
